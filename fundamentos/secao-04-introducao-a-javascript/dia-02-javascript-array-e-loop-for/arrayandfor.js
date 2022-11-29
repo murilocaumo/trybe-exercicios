9b0588df2c1f5c08173ff20c68212b427e1fe61b
murilo <muriloccaumo@gmail.com>
2022-11-29 15:44:43 -0300

    dia 4-2

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/arrayandfor.js
@@ -0,0 +1,64 @@
+// Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 0; index < numbers.length ; index += 1) {
+// console.log(numbers[index]);
+// }
+
+
+
+
+
+// Some todos os valores contidos no array e imprima o resultado;
+
+// let soma = 0;
+// for (let array = 0 ; array < numbers.length ; array +=1 )
+// { 
+//     soma += numbers[array];
+// }
+// console.log(soma);
+
+
+
+
+
+// Calcule e imprima a média aritmética dos valores contidos no array;
+
+// let soma = 0;
+// for (let array = 0 ; array < numbers.length ; array +=1 )
+// { 
+//     soma += numbers[array];}
+//     let media = soma / numbers.length
+//     console.log (media.toFixed(0))
+
+
+
+
+
+
+// Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+// let soma = 0;
+// for (let array = 0 ; array < numbers.length ; array +=1 )
+// { 
+//     soma += numbers[array];}
+//     let media = soma / numbers.length
+//     console.log (media.toFixed(0))
+
+// if (media > 20) {
+// console.log("valor maior que 20");
+// }
+// else { (media <= 20) 
+// console.log("valor menor ou igual a 20");
+// }
+
+
+
+
+
+// Utilizando for, descubra qual o maior valor contido no array e imprima-o
+
+for (let index = 0; index < numbers.length; index += 1) {
+    const element = numbers[index];
+}
