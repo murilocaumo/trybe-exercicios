ecc4c254d2f96f321a2df9eb301276d42b5a7f3d
murilo <muriloccaumo@gmail.com>
2022-12-01 18:48:53 -0300

    1 pagina de exercicios feitas

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/exercicio.leitura.js
@@ -0,0 +1,14 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  console.log('O livro favorito de Julia Pessoa se chama ' + leitor['livrosFavoritos'])
+
